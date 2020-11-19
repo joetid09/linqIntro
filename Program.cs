b0046525447473b3baf8c5c47c7efaf87ba10c5e
joseph tidwell <joseph.tidwell09@gmail.com>
2020-11-19 11:47:40 -0600

    filtered fruits that start with L out of list

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find the words in the collection that start with the letter 'L'
+
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            IEnumerable<string> LFruits = from fruit in fruits
+                                          where fruit.StartsWith("L")
+                                          orderby fruit ascending
+                                          select fruit;
+
+            foreach (string f in LFruits)
+            {
+                Console.WriteLine($"{f}");
+            }
+
+        }
+
+    }
+}
